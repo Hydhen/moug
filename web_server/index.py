678e065b4c3080e6f473d9d2b695d29f2f5b9a01
Loïc Juillet <juillet.loic@gmail.com>
2016-05-18 11:52:10 +0200

    handle RED/BLUE team system - remove thread

--- web_server/index.py
+++ web_server/index.py
@@ -10,34 +10,18 @@ app                 = Flask(__name__)
 CLIENTS             = []
 STATUS              = False
 TIME                = None
-LAST_UPDATE         = None
-TIME_LOCK           = thread.allocate_lock()
 SCORE_RED           = 0
+RED_TEAM            = []
 SCORE_BLUE          = 0
-
-def HandleGame():
-    global TIME, TIME_LOCK
-    now = datetime.now()
-
-    while True :
-        LAST_UPDATE = now
-        offset = now - TIME
-        print "HandleGame: " + "Game has been started for " + str(offset.seconds / 60) + "m " + str(offset.seconds % 60) + "s"
-        sleep(5)
+BLUE_TEAM           = []
+NEXT_TEAM           = "RED"
 
 @app.route("/status", methods=['GET'])
 def Status():
     global CLIENTS, STATUS, TIME, TIME_LOCK, LAST_UPDATE
     content = "Something went wrong"
     time = datetime.now()
-    last_lock = None
-    last_lock_str = ""
 
-    last_lock = LAST_UPDATE
-    if last_lock is not None :
-        last_lock_str = str(last_lock.second / 60) + "m " + str(last_lock.second % 60) + "s"
-    else :
-        last_lock_str = "Never"
     if STATUS == True :
         offset = time - TIME
         content = "Game has been started for " + str(offset.seconds / 60) + "m " + str(offset.seconds % 60) + "s"
@@ -45,8 +29,7 @@ def Status():
         content = "No game instantiated"
     return jsonify(content=content,
                    clients=CLIENTS,
-                   status=STATUS,
-                   lastUpdate=last_lock_str)
+                   status=STATUS)
 
 @app.route("/stop", methods=['GET'])
 def Stop():
@@ -71,45 +54,91 @@ def Start():
         TIME = datetime.now()
         STATUS = True
         content = "Game start"
-        thread = threading.Thread(target=HandleGame)
-        thread.start()
     else :
         content = "Game has already started"
     return jsonify(content=content)
 
 @app.route("/list", methods=['GET'])
 def List():
-    global CLIENTS
+    global CLIENTS, RED_TEAM, BLUE_TEAM
 
     return jsonify(content="list",
                    traffic=len(CLIENTS),
-                   clients=CLIENTS)
+                   clients=CLIENTS,
+                   teamBlue=BLUE_TEAM,
+                   teamRed=RED_TEAM)
 
-@app.route("/connexion", methods=['GET'])
-def Connexion():
+@app.route("/connection", methods=['GET'])
+def Connection():
+    global CLIENTS, RED_TEAM, BLUE_TEAM, NEXT_TEAM
+    content = "Something went wrong..."
+    team = ""
+
+    client = request.environ['REMOTE_ADDR']
+    if client == '127.0.0.1' :
+        content = "Welcome home"
+    else :
+        if client in CLIENTS :
+            content = "Already connected"
+        else :
+            CLIENTS.append(client)
+            content = "Connected as "
+            if NEXT_TEAM == "RED" :
+                RED_TEAM.append(client)
+                NEXT_TEAM = "BLUE"
+                content = content + "RED"
+                team = "RED"
+            elif NEXT_TEAM == "BLUE" :
+                BLUE_TEAM.append(client)
+                NEXT_TEAM = "RED"
+                content = content + "BLUE"
+                team = "BLUE"
+            else :
+                content = "Something went wrong with NEXT_TEAM..."
+    return jsonify(content=content,
+                   team=team)
+
+@app.route("/disconnection", methods=['GET'])
+def Disconnection():
     global CLIENTS
     content = "Something went wrong..."
 
     client = request.environ['REMOTE_ADDR']
-    if client in CLIENTS :
-        content = "Already connected"
+    if client == '127.0.0.1' :
+        content = "Welcome home"
     else :
-        CLIENTS.append(client)
-        content = "Connected"
+        if client in CLIENTS :
+            CLIENTS.remove(client)
+            content = "Disconnected from "
+            if client in RED_TEAM :
+                RED_TEAM.remove(client)
+                content = content + "RED"
+            elif client in BLUE_TEAM :
+                BLUE_TEAM.remove(client)
+                content = content + "BLUE"
+            else :
+                content = content + "NEUTRAL"
+        else :
+            content = "Was not connected"
     return jsonify(content=content)
 
-@app.route("/disconnexion", methods=['GET'])
-def Disconnexion():
-    global CLIENTS
+@app.route("/", methods=['GET'])
+def Index():
+    global CLIENTS, RED_TEAM, BLUE_TEAM, STATUS, TIME
     content = "Something went wrong..."
+    time = datetime.now()
 
-    client = request.environ['REMOTE_ADDR']
-    if client in CLIENTS :
-        CLIENTS.remove(client)
-        content = "Disconnected"
+    if STATUS == True :
+        if TIME is not None :
+            offset = now - TIME
+            content = "Game has started " + str(offset.seconds / 60) + "m " + str(offset.seconds % 60) + "s ago"
     else :
-        content = "Was not connected"
-    return jsonify(content=content)
+        content = "No game instantiated"
+    return jsonify(content=content,
+                   traffic=len(CLIENTS),
+                   teamRed=RED_TEAM,
+                   teamBlue=BLUE_TEAM,
+                   status=STATUS)
 
 if __name__ == "__main__":
     app.debug = True
