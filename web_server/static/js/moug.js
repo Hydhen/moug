36b74374aea53ddd776030036fd5a8745daca653
Loïc Juillet <juillet.loic@gmail.com>
2016-05-31 14:36:37 +0200

    grown stroke and trail width, removed alert

--- web_server/static/js/moug.js
+++ web_server/static/js/moug.js
@@ -1,12 +1,12 @@
 window.onload = function onLoad() {
     var circle = new ProgressBar.Circle('#timer', {
         color: '#FFB600',
-        strokeWidth: 11,
-        trailWidth: 11,
+        strokeWidth: 20,
+        trailWidth: 20,
         duration: 3000,
     });
 
     circle.animate(1, {}, function() {
-        alert("Game over");
+        // TODO WHEN GAME IS OVER
     });
 };
--- web_server/static/js/utils-video.js
+++ web_server/static/js/utils-video.js
@@ -8,7 +8,6 @@ document.getElementById('video').addEventListener('ended', nextStep, false);
 
 function nextStep(e) {
     if (videoIndex > (videoList.length - 1)) {
-        alert('Begin Game');
         window.location.href = "/moug";
     }
     $('video').attr('src', '/static/mp4/' + videoList[videoIndex] + '.mp4');
