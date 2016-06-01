8896b3ffc34732e27ee80bc5ed19b8b1af14e9f9
Loïc Juillet <juillet.loic@gmail.com>
2016-06-01 17:57:30 +0200

    reformat code for better adaptation

---
+++ web_server/static/js/tutorial.js
@@ -0,0 +1,18 @@
+var videoIndex = 0;
+var videoList = [
+    'introduction',
+    'instruction-1',
+    'instruction-2',
+    'team',
+];
+
+document.getElementById('video').addEventListener('ended', nextStep, false);
+
+function nextStep(e) {
+    if (videoIndex > (videoList.length - 1)) {
+        window.location.href = "/moug";
+    }
+    $('video').attr('src', '/static/mp4/' + videoList[videoIndex] + '.mp4');
+    videoIndex = videoIndex + 1;
+    $('video').load();
+}
